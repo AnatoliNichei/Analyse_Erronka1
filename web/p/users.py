fe6bff53aa5eb7bcb811f84d4209f9df440fe152
Sebastian <sebasmarq@protonmail.com>
2021-09-27 13:10:46 +0200

    Reorganizar

--- web/p/users.py
+++ web/p/users.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
         import read_html
     else:
         from .. import read_html
-    read_html.read_template('../source/users.json', "Content-Type: application/json;charset=utf-8")
+    read_html.read_template('users.json', "Content-Type: application/json;charset=utf-8")
