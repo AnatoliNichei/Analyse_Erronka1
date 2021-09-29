aefe45fa39bbd929940f526c7cad4e27e95b456f
AnatoliNichei <nichei.anatoli@uni.eus>
2021-09-29 12:17:25 +0200

    Productos_Añadidos

--- web/CRUD.py
+++ web/CRUD.py
@@ -15,7 +15,7 @@ def taulak_sortu():
     cursor.execute(
         "CREATE TABLE  bezeroa(Erabiltzailea varchar(255) PRIMARY KEY , Izena VARCHAR(255),Abizena VARCHAR(255),Nan varchar(255), Helbidea varchar(255), Telefono INT, Pasahitza varchar(255), Emaila varchar(255))");
     cursor.execute(
-        "CREATE TABLE  produktua(ProduktuKodea varchar(255) PRIMARY KEY , Izena VARCHAR(255),Prezioa DOUBLE , Irudia varchar(255), Iruzkina varchar(255))");
+        "CREATE TABLE  produktua(ProduktuKodea varchar(255) PRIMARY KEY , Izena VARCHAR(255),Prezioa DOUBLE , Irudia varchar(255), Iruzkina varchar(255), Mota varchar(255))");
     cursor.execute(
         "CREATE TABLE  saskia(SaskiKodea varchar(255) PRIMARY KEY,Erabiltzailea varchar(255),FOREIGN KEY (Erabiltzailea) REFERENCES bezeroa(Erabiltzailea) ON UPDATE CASCADE)");
     cursor.execute(
@@ -57,10 +57,10 @@ def bezeroa_eguneratu(erabiltzailea, **zelaiak):
     conexion.commit()
 
 
-def gehitu_produktua(produktukodea, izena, prezioa, irudia, iruzkina):
-    cursor.execute("INSERT INTO produktua(ProduktuKodea,Izena,Prezioa,Irudia,iruzkina) "
-                   "VALUES (%s, %s, %s, %s, %s)",
-                   (produktukodea, izena, prezioa, irudia, iruzkina))
+def gehitu_produktua(produktukodea, izena, prezioa, irudia, iruzkina, mota):
+    cursor.execute("INSERT INTO produktua(ProduktuKodea,Izena,Prezioa,Irudia,iruzkina,mota) "
+                   "VALUES (%s, %s, %s, %s, %s, %s)",
+                   (produktukodea, izena, prezioa, irudia, iruzkina, mota))
     conexion.commit()
 
 
@@ -132,8 +132,18 @@ def eskaera_eguneratu(produktu_kodea, saski_kodea, kantitate_berria):
 def datuak_inizializatu():
     gehitu_bezeroa("AlvaroCazador", "Alvaro", "Viguera", "34567823D", "Legaño 6 3D Berriz", 456789221, "1234",
                    "viguera.alvaro@uni.eus")
-    gehitu_produktua("GA01", "Galleta1", 1.3, "blog-img-01.jpg", "Una galleta fea, pero el sabor y la textura son los mejores.")
-    gehitu_produktua("GA02", "Galleta2", 3, "blog-img-02.jpg", "Una galleta bonita, una experiencia exquisita.")
+    gehitu_produktua("GA01", "Apple Cookie", 0.75, "images/Cookie/appleCookie.png", "Sagarraz eginezko gaileta gozoak.", "Fruta")
+    gehitu_produktua("GA02", "Banana Cookie", 7.50, "images/Cookie/bananaCookie.png", "Canarias-eko platanoekin eginezko gaileta gozoak. La Palman labetuta.", "Simple")
+    gehitu_produktua("GA03", "Cashew Cookie", 1.49, "images/Cookie/cashewCookie.png", "Anakardoz eginezko gaileta gozoak.", "Fruta")
+    gehitu_produktua("GA04", "Eclipse Cookie", 0.39, "images/Cookie/eclipseCookie.png", "Gaileta hauek erdi sinpleak eta erdi txokolatezkoak dira.", "Chocolate")
+    gehitu_produktua("GA05", "Gingerbread Cookie", 5.39, "images/Cookie/gingerbreadCookie.png", "Gabonetara arte itxaron nahi ez baduzu, erosi orain jengibre-gailetak.", "Simple")
+    gehitu_produktua("GA06", "Ladyfinger Cookie", 0.20, "images/Cookie/ladyfingerCookie.png", "Txokolatean bustitzeko gaileta ederra.", "Simple")
+    gehitu_produktua("GA07", "Madaleines", 3.90, "images/Cookie/madaleinesCookie.png", "Eclipse bezalako gailetak, baina politagoak eta gareztiagoak.", "Chocolate")
+    gehitu_produktua("GA08", "Pear Cookie", 1.15, "images/Cookie/pearCookie.png", "Madariz eginezko gaileta gozoak.", "Fruta")
+    gehitu_produktua("GA09", "Pinwheel Cookie", 0.80, "images/Cookie/pinwheelCookie.png", "Kiribil itxura duten gaileta gozoak.", "Chocolate")
+    gehitu_produktua("GA10", "Raspberry Cookie", 2.95, "images/Cookie/raspberryCookie.png", "Mugurdiz eginezko gaileta gozoak.", "Fruta")
+    gehitu_produktua("GA11", "Walnut Cookie", 1.00, "images/Cookie/walnutCookie.png", "Intxaurrez eginezko gaileta gozoak.", "Simple")
+    gehitu_produktua("GA12", "White Chocolate Cookie", 0.59, "images/Cookie/whiteChocoCookie.png", "Lidl-etik lapurtutako gaileta zakarrak.", "Chocolate")
     gehitu_saskia("001", "AlvaroCazador")
     gehitu_eskaera("GA01", "001", 4)
 
