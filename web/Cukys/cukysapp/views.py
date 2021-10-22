92bda343b0b34e7f67a54a0f94f05f0d1c85ec8d
AlvaroViguera18 <alvarovigueradonaire@gmail.com>
2021-10-22 10:48:42 +0200

    hasierako index

--- web/Cukys/cukysapp/views.py
+++ web/Cukys/cukysapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.utils import timezone
 
-# Create your views here.
+from cukysapp.models import *
+from django.shortcuts import redirect
+
+
+def post_list(request):
+    #posts=Post.objects.filter(slug="ez")
+    # posts = Post.objects.all()
+   # return render(request,'../templates/blog/post_list.html',{'posts':posts})
+    return render(request, 'cukys/index.html')
