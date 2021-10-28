9112abd7db3ad0683a5f5df6b6e6c5dbea3c4e93
root <root@UH-108-11.uni.lan>
2021-10-28 13:58:01 +0200

    test if import-export exists

--- web/Cukys/cukysapp/admin.py
+++ web/Cukys/cukysapp/admin.py
@@ -1,34 +1,38 @@
 from django.contrib import admin
 from .models import *
-from import_export.admin import ImportExportModelAdmin
+found = True
+try:
+    from import_export.admin import ImportExportModelAdmin
+except ImportError:
+    found = False
 
 # Register your models here.
+if found:
+
+    class BezeroaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
 
 
-class BezeroaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    ...
+    class SaskiaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
 
 
-class SaskiaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    ...
+    class MotaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
 
 
-class MotaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    ...
+    class ProduktuaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
 
 
-class ProduktuaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    ...
-
-
-class EskaeraAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    ...
+    class EskaeraAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
 
 
 
-admin.site.register(Bezeroa, BezeroaAdmin)
-admin.site.register(Saskia, SaskiaAdmin)
-admin.site.register(Mota, MotaAdmin)
-admin.site.register(Produktua, ProduktuaAdmin)
-admin.site.register(Eskaera, EskaeraAdmin)
+    admin.site.register(Bezeroa, BezeroaAdmin)
+    admin.site.register(Saskia, SaskiaAdmin)
+    admin.site.register(Mota, MotaAdmin)
+    admin.site.register(Produktua, ProduktuaAdmin)
+    admin.site.register(Eskaera, EskaeraAdmin)
 
