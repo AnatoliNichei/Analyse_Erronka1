63a6e85221937f50d269051040b26df99ab91932
root <root@UH-108-11.uni.lan>
2021-11-15 12:49:39 +0100

    fix

--- web/Cukys/Cukys/settings.py
+++ web/Cukys/Cukys/settings.py
@@ -138,11 +138,16 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'infocukys@gmail.com'
-EMAIL_HOST_PASSWORD = 'cukys_1234'
-EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'infocukys@gmail.com'
+#EMAIL_HOST_PASSWORD = 'cukys_1234'
+#EMAIL_USE_TLS = True
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
