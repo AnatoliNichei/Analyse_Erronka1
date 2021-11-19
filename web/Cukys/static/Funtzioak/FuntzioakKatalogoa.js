0f2ce8e5e0a4be504cb02eafc7cd55a10b7fdf2d
AnatoliNichei <nichei.anatoli@uni.eus>
2021-11-19 10:17:57 +0100

    datu basea

--- web/Cukys/static/Funtzioak/FuntzioakKatalogoa.js
+++ web/Cukys/static/Funtzioak/FuntzioakKatalogoa.js
@@ -3,9 +3,11 @@ const galletaGuztiak = document.querySelectorAll(".galletaGuztiak");
 
 for (i = 0; i < btns.length; i++){
 
+    let element = btns[i];
     btns[i].addEventListener("click", (e) =>{
         e.preventDefault();
         const filter = e.target.dataset.filter;
+        element.classList.add("btnFilterFocus");
         galletaGuztiak.forEach((product)=>{
             if (product.classList.contains("d-flex")){
                     product.classList.toggle("d-flex")
